00867a8d92f12e9a96c4a1ae0c7385fab1850c1b
Nikhil Barman <balubhai700a@gmail.com>
2022-07-31 16:59:49 +0530

    completed

---
+++ src/Bar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './bar.css';
+function Bar() {
+  return (
+    <div className='heading'>
+                
+    <p className='coin-name'>Coin </p>
+    <p className='price'>price</p>
+    <p className='vol'>Volume</p>
+    <p className='per'>24h</p>
+    <p className='mkt'>Mkt Cap</p>
+    
+   
+
+</div>
+  )
+}
+
+export default Bar
